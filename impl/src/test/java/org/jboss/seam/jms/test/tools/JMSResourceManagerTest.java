85a5020ac5442a0a3ebb24c24a91ca1be4681084
John Ament <john.d.ament@gmail.com>
2011-03-31 18:56:33 -0400

    Fixed package name of test case.

--- impl/src/test/java/org/jboss/seam/jms/test/tools/JMSResourceManagerTest.java
+++ impl/src/test/java/org/jboss/seam/jms/test/tools/JMSResourceManagerTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jboss.seam.jms.tools.test;
+package org.jboss.seam.jms.test.tools;
 
 import javax.annotation.Resource;
 import javax.enterprise.event.Event;
